70678df036f3420525bf1a55fb05e78d9b7b0fe7
Maddie <32415621+SpyHoodle@users.noreply.github.com>
2022-11-03 08:47:26 +0000

    extending structs

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,32 @@
-struct Buffer {
+enum Mode {
+    Normal,
+    Insert,
+    Select
+}
+
+struct Buffer<'a> {
     data: Vec<String>,
-    name: &str,
+    name: &'a str,
+    path: &'a str,
 }
 
 struct Editor {
     buffer: Buffer,
+    cursor: [i32, 2],
+    mode: Mode;
 }
 
 fn main() {
     let bufffer = Buffer {
-        data: Vec::from([String::from("a test")]),
-        name: "uhh"
+        data: Vec::from([String::from("Hello"), String::from("World")]),
+        name: "[No Name]",
+        path: "/home/spy",
     };
 
     let editor = Editor {
         buffer: bufffer,
+        cursor: [0, 0],
+        mode: Mode::Normal,
     };
 
     println!("{}", editor.buffer.data[0])
